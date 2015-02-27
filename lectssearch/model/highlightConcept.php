196f8148e5e2dd1a21dcdf1115fa26fefe48c461
kzintun <kyawzin.kzin@gmail.com>
2015-02-27 12:41:52 +0800

    shorten text on search result

--- lectssearch/model/highlightConcept.php
+++ lectssearch/model/highlightConcept.php
@@ -5,7 +5,7 @@
 // highlight the keywords
 function highlightConcept($haystackArray) {
 	
-	//echo '<pre>';
+	echo '<pre>';
 	//for ($k=0 ; $k < sizeof($haystackArray); $k++){
 	foreach ($haystackArray as $key => $hay){
 	
@@ -17,13 +17,23 @@ function highlightConcept($haystackArray) {
 		//print_r($wordArry);
 		
 		for ($i=$startWIndex; $i<= $endWIndex ; $i++){
-			$wordArry[$i] = '<b>'.$wordArry[$i].' </b>';
+			$wordArry[$i] = '<b>'.$wordArry[$i].'</b>';
+		//	$wordArry[$i] = '<b> '.$wordArry[$i].' </b>';
 			//echo $wordArry[$i] .' ';
 		}
+		
+		// shorten the length of the sentence to 16 words
+		
 		$haystackArray[$key]['text'] = implode(" ",$wordArry);
+		preg_match_all('((?:\S+\s*){0,7}'.$wordArry[$startWIndex].'(?:\s*\S+){0,7})',$haystackArray[$key]['text'],$matches);
+		//((?:\S+\s*){0,5}name(?:\s*\S+){0,5})
+		//preg_match_all('/(?:\S+\s*){0,7}\S*'.$wordArry[$startWIndex].'\S*(?:\s*\S+){0,7}/s',$haystackArray[$key]['text'],$matches);
+		//var_dump($wordArry[$startWIndex]);
+		//var_dump($matches[0][0]);
+		$haystackArray[$key]['text'] = $matches[0][0];
 		//echo $hay['text'] . ' ';
 	}
-	//echo '</pre>';
+	echo '</pre>';
     return $haystackArray;
 }
 ?>
